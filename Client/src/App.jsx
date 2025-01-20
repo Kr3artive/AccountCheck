2af8d2463416a2cece8438faf78f0b34e618b8ef
Abiye <abiyerowland@gmail.com>
2025-01-20 17:58:02 +0200

    Updated Endpoint

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
 
   useEffect(() => {
     axios
-      .get("http://localhost:9000/listbanks")
+      .get("https://accountcheckapi.onrender.com/listbanks")
       .then((response) => setData(response.data))
       .catch((error) => console.error("Error fetching data", error));
   }, []);
@@ -25,7 +25,7 @@ const App = () => {
     };
     try {
       const response = await axios.post(
-        "http://localhost:9000/getname",
+        "https://accountcheckapi.onrender.com/getname",
         payload
       );
       console.log(response.data);
